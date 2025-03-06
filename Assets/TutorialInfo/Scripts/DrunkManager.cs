ec2bd0dbd530ebfe0eb5492cd84da169e04e9271
EarthToMaher <maher.m.3rdlawgames@gmail.com>
2025-03-05 22:28:33 -0500

    drunkness effects glasses, now has a base layer that will be hidden from view as you get more drunk

--- Assets/TutorialInfo/Scripts/DrunkManager.cs
+++ Assets/TutorialInfo/Scripts/DrunkManager.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class DrunkManager : MonoBehaviour
 {
     public float drunkness;
+    [SerializeField] private Material glasses;
 
     public void ChangeDrunkness(float drunkChange)
     {
         drunkness = Mathf.Clamp01(drunkness+drunkChange);
-        //TODO: Apply drunk level to stencil
+        glasses.color = new Color(glasses.color.r,glasses.color.g,glasses.color.b,drunkness);
     }
 
     public float GetDrunkness()
