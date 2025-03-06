14d523d034be132cb1c7f9cfecc675928f5c1158
EarthToMaher <maher.m.3rdlawgames@gmail.com>
2025-03-06 13:52:21 -0500

    weird bug fix

--- Assets/TutorialInfo/Scripts/DrunkManager.cs
+++ Assets/TutorialInfo/Scripts/DrunkManager.cs
@@ -4,7 +4,10 @@ public class DrunkManager : MonoBehaviour
 {
     public float drunkness;
     [SerializeField] private Material glasses;
-
+    private void Start() 
+    {
+        glasses.color = new Color(glasses.color.r,glasses.color.g,glasses.color.b,drunkness);
+    }
     public void ChangeDrunkness(float drunkChange)
     {
         drunkness = Mathf.Clamp01(drunkness+drunkChange);
