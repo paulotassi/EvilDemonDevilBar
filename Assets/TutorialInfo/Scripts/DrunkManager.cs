2a94e6f3e8a5d9d2a8307608ccc15c30e7c79b92
paulotassi <133156624+paulotassi@users.noreply.github.com>
2025-03-17 11:46:05 -0400

    added audio stuff

--- Assets/TutorialInfo/Scripts/DrunkManager.cs
+++ Assets/TutorialInfo/Scripts/DrunkManager.cs
@@ -8,14 +8,19 @@ public class DrunkManager : MonoBehaviour
     [SerializeField] private Material glassesShade;
     [SerializeField] private float maxShade;
     [SerializeField] private float minShade;
+    [SerializeField] private AudioSource monsterSounds;
+    [SerializeField] private AudioSource cupSoundSource;
+    [SerializeField] private AudioClip drinkingSound;
     private void Start() 
     {
         glasses.color = new Color(glasses.color.r,glasses.color.g,glasses.color.b,drunkness);
     }
     public void ChangeDrunkness(float drunkChange)
     {
+        cupSoundSource.PlayOneShot(drinkingSound);
         drunkness = Mathf.Clamp01(drunkness+drunkChange);
         glasses.color = new Color(glasses.color.r,glasses.color.g,glasses.color.b,drunkness);
+        monsterSounds.volume = drunkness;
     }
 
     public float GetDrunkness()
